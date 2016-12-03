3c26670ed79519a2a4ec7ae8da6e8193effa7c01
Raymundo Peralta <movile@peta.mx>
2016-12-02 23:33:41 -0600

    primer commit

---
+++ LoginFB/BridgeHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgeHeader.h
+//  LoginFB
+//
+//  Created by Raymundo Peralta on 11/4/16.
+//  Copyright © 2016 Raymundo Peralta. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* BridgeHeader_h */
--- LoginFB/ViewController.swift
+++ LoginFB/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // let unRect = self.view.bounds
+        logoutFB()
+        
+        if FBSDKAccessToken.currentAccessToken() != nil {
+            // esta app ya fue autorizada, no hay que fastidiar al usuario
+            print("Ya entre")
+            let logout = UIButton(type: .Custom)
+            logout.setTitle("Logout", forState:.Normal)
+            logout.center = self.view.center
+            logout.addTarget(self, action: #selector(ViewController.logoutFB), forControlEvents:.TouchUpInside)
+        } else {
+            FBSDKLoginManager().loginBehavior = .Web
+            let login = FBSDKLoginButton()
+            
+            login.center = self.view.center
+            //let login:FBSDKLoginButton = FBSDKLoginButton(frame:CGRectMake(unRect.width/3, unRect.size.height/3, unRect.size.width / 3, unRect.size.height / 3))
+            self.view.addSubview(login)
+            login.delegate = self
+            login.readPermissions = ["public_profile", "email"]
+        }
+    }
+    
+    func logoutFB() {
+        FBSDKLoginManager().logOut()
+        FBSDKAccessToken.setCurrentAccessToken(nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error != nil {
+            print("no se puede completar login con fb")
+        } else  if result.isCancelled {
+            print("El usuario no acepta el login con facebook")
+        } else
+        {
+            if result.grantedPermissions.contains("email") {
+                let email = result.valueForKey("email") as! String
+            
+                print("el usuario tiene el correo \(email)")
+            }
+            print("pase por el resultado")
+            
+        }
+    }
+    
 
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User loged out")
+    }
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                print("User Email is: \(userEmail)")
+            }
+        })
+    }
+    
 }
 
